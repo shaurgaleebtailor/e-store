2ca319e0634b40de46c6251e08abc1220ac57d0a
shaurgaleebtailor <galeeb233@gmail.com>
2025-02-15 17:29:08 +0530

    added strictly support for mow variation

--- src/components/utilities/useMOWChecker.js
+++ src/components/utilities/useMOWChecker.js
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 export const useMOWChecker = () => {
-  // letting MOW styles for the screens which are less than 768px
+  // letting MOW styles for the screens which are less than 768px(mimicking as MOW)
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   useEffect(() => {
     const sizeHandler = () => {
-      setIsMobile(window.innerWidth <= 768);
+      // strictly, letting mow styles when its in <= 480px
+      let isRealMOW = window.matchMedia("(max-width:480px)").matches;
+      if (isRealMOW) {
+        setIsMobile(isRealMOW);
+      } else {
+        setIsMobile(window.innerWidth <= 768);
+      }
     };
     window.addEventListener("resize", sizeHandler);
     return () => window.removeEventListener("resize");
