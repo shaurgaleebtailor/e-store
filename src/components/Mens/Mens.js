558bc92b81167a10711ebf6a655bc1c7dfd59577
shaurgaleebtailor <galeeb233@gmail.com>
2025-02-18 00:10:27 +0530

    added mens-category intro

--- src/components/Mens/Mens.js
+++ src/components/Mens/Mens.js
@@ -1,15 +1,52 @@
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
-const Mens = ()=>{
-    const navigator = useNavigate();
-    const backToStore = ()=>{
-        // navigate back to store
-        navigator("/")
-      }
-    return <Fragment>
+import { clsx as cn } from "clsx";
+import { useMOWChecker } from "../utilities/useMOWChecker.js";
+import menscategoryintro from "../../assets/mens/mens-category-intro.webp";
+import "./Mens.scss";
+const Mens = () => {
+  const isMobile = useMOWChecker();
+  const navigator = useNavigate();
+  const backToStore = () => {
+    // navigating back to store and maintaining the scroll position(overiding the previous scroll retentation) at the top,
+    // since React Router by default retains the scroll position(old scroll position) when navigating between pages.
+    window.scrollTo(0, 0);
+    navigator("/");
+  };
 
-        <h1>Mens Page</h1>
-        <button onClick={backToStore}>Store</button>
+  return (
+    <Fragment>
+      <div className="mens-category">
+        <h1>
+          <i>Men's Category</i>
+        </h1>
+        <div className={cn("mens-category-intro", { mow: isMobile })}>
+          <img
+            src={menscategoryintro}
+            alt="mens category"
+            className="mens-category-intro-img"
+          />
+          <div className="mens-category-intro-info">
+            <h2>Explore Men's Fashion</h2>
+            <p>
+              Upgrade your style with our latest collection of men's clothing.
+              From casual wear to formal attire, we bring you premium-quality
+              outfits designed for comfort and confidence. Whether you're
+              looking for everyday essentials or statement pieces, find the
+              perfect fit for every occasion.
+            </p>
+            <p>
+              Discover top trends, durable fabrics, and stylish designs tailored
+              to enhance your wardrobe. Shop now and redefine your fashion game
+              with outfits that make a lasting impression!
+            </p>
+          </div>
+        </div>
+        <div id="back-to-store">
+          <button onClick={backToStore}>&larr; Store</button>
+        </div>
+      </div>
     </Fragment>
-}
-export default Mens
+  );
+};
+export default Mens;
